20c4de6cfbbcd473fc585c68f3695bc49a3f89f1
Amaan Asim <amaanasim09@gmail.com>
2025-08-01 18:14:16 -0400

    Initial Commit

---
+++ PokeiOSTests/PokeiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokeiOSTests.swift
+//  PokeiOSTests
+//
+//  Created by Amaan Asim on 2025-08-01.
+//
+
+import Testing
+@testable import PokeiOS
+
+struct PokeiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
